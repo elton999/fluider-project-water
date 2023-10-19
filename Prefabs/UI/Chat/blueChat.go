642fd7b6a8ab83a225dbbdfc6b0c2ad4c43c1c33
Elton Silva <eltoncarsilva73@gmail.com>
2023-10-19 19:01:28 -0300

    test spwan of chat

---
+++ Prefabs/UI/Chat/blueChat.go
@@ -0,0 +1,15 @@
+components {
+  id: "chat_ballon_blue"
+  component: "/Prefabs/UI/Chat/chat_ballon_blue.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
